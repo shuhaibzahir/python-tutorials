1ddc808a6f7b595e8a8bd0c04bebaa7c211a7fe8
shuhaibzahir <yesshuhaib.ss@gmail.com>
2022-09-17 09:33:19 +0530

    match case in python

---
+++ switch_case.py
@@ -0,0 +1,24 @@
+ #Using the ‘switch case’ write a program to accept an input number from the user and output the day as follows. 
+ 
+from email.policy import default
+
+
+number = int(input("enter your input"))
+
+match number:
+    case 1:
+        print("monday")
+    case 2:
+        print("tuesday")
+    case 3:
+        print("wednesday")
+    case 4:
+        print("thursday")
+    case 5:
+        print("friday")
+    case 6:
+        print("saturday")
+    case 7:
+        print("sunday")
+    case _:
+        print("not valid entry")
